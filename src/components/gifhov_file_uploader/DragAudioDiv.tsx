da753bcd4afd24669293af8977239bd8ef875812
maxnelson <maxnelson88@gmail.com>
2024-04-26 13:27:12 -0700

    fixing up file uploader front end

--- src/components/gifhov_file_uploader/DragAudioDiv.tsx
+++ src/components/gifhov_file_uploader/DragAudioDiv.tsx
@@ -1,7 +1,45 @@
+import { useState } from "react";
 export function DragAudioDiv() {
+  const [draggedOver, setDraggedOver] = useState("");
+
+  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log("file dropped");
+  };
+  const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log("dragging over");
+  };
+  const handleDragEnter = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (e.dataTransfer.items[0].type === "image/gif") {
+      setDraggedOver("valid");
+      return;
+    } else {
+      setDraggedOver("invalid");
+    }
+  };
+  const handleDragLeave = (e: React.DragEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    console.log("dragging leave");
+  };
+
   return (
     <>
-      <div className="drag_file_div">
+      <div
+        className={
+          draggedOver === "valid"
+            ? "fileUploadInputDiv fileUploadInputDiv_draggedOverValid"
+            : draggedOver === "invalid"
+            ? "fileUploadInputDiv fileUploadInputDiv_draggedOverInvalid"
+            : "fileUploadInputDiv"
+        }
+        onDrop={(e) => handleDrop(e)}
+        onDragOver={(e) => handleDragOver(e)}
+        onDragEnter={(e) => handleDragEnter(e)}
+        onDragLeave={(e) => handleDragLeave(e)}
+      >
         <p>drag audio</p>
       </div>
     </>
